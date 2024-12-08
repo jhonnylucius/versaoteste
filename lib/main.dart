6c13575efc5996d08db17f6856a23d58221731bd
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2024-12-08 16:34:50 -0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
-// Importa o pacote Flutter para inicialização.
 import 'package:flutter/material.dart';
 
-// Importa arquivos de navegação.
 import 'navigation/app_router.dart';
 import 'navigation/navigation_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-// Widget principal do aplicativo.
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AgendaPro', // Título do aplicativo.
+      title: 'AgendaPro',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      navigatorKey:
-          NavigationService.navigatorKey, // Configura navegação global.
-      onGenerateRoute: AppRouter.generateRoute, // Define como gerar as rotas.
-      initialRoute: '/login', // Define a rota inicial como login.
+      darkTheme: ThemeData.dark(),
+      navigatorKey: NavigationService.navigatorKey,
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: '/login',
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:versaoteste/main.dart';
+import 'package:versaoteste/navigation/app_router.dart';
+import 'package:versaoteste/navigation/navigation_service.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  runApp(const App());
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class App extends StatelessWidget {
+  const App({super.key});
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AgendaPro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.dark(),
+      navigatorKey: NavigationService.navigatorKey,
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: '/login',
+    );
+  }
 }
